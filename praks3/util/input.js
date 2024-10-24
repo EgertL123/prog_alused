7ac8039254df565f3e4e545bc30da482c0633674
Egert Linnas <egert.linnas@voco.ee>
2024-10-24 12:07:51 +0300

    utils

---
+++ praks3/util/input.js
@@ -0,0 +1,9 @@
+import readline from 'node:readline';
+
+const readline = require('node:readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+export default rl
